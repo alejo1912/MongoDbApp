303d6f2b6a916af219cf933dacd353719aaa8e1f
MAURICIO BELLO <bello900@hotmail.com>
2022-01-04 23:10:25 -0500

    creacion entidades del modelo app

---
+++ MongoDbApp/Models/Deportistas.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDbApp.Models
+{
+    public class Deportistas
+    {
+        public string _id { get; set; }
+
+        /// <summary>
+        /// Nombre juados
+        /// </summary>
+        public string nombre { get; set; }
+
+        /// <summary>
+        /// numero camiseta
+        /// </summary>
+        public int numero { get; set; }
+
+        /// <summary>
+        /// posision en el area de juego ejemplo: DC defensa central
+        /// </summary>
+        public string posision { get; set; }
+
+        /// <summary>
+        /// identificacion del jugador
+        /// </summary>
+        public string documento { get; set; }
+
+        /// <summary>
+        /// id automatico de la tabla de equipos
+        /// </summary>
+        public string idEquipo { get; set; }
+    }
+}
