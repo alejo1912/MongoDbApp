28336e8652b86e3f1dc15d6233f7b5003730c427
MAURICIO BELLO <bello900@hotmail.com>
2022-01-08 18:26:10 -0500

    ArbitrosRepositorioCollection

--- MongoDbApp/Repositorio/ArbitrosES/ArbitrosRepositorioCollection.cs
+++ MongoDbApp/Repositorio/ArbitrosES/ArbitrosRepositorioCollection.cs
@@ -9,8 +9,51 @@ using System.Threading.Tasks;
 
 namespace MongoDbApp.Repositorio.ArbitrosES
 {
-    public class ArbitrosRepositorioCollection
+    public class ArbitrosRepositorioCollection : IArbitrosContradoCollection
     {
+        internal MongoDBRepository _repository = new MongoDBRepository();
+        private IMongoCollection<Arbitros> collectin;
+        public ArbitrosRepositorioCollection()
+        {
+            // si no encuentra la collection crea una nueva
+            collectin = _repository.db.GetCollection<Arbitros>("Arbitros");
+        }
+        public async Task DeleteArbitro(string id)
+        {
+            var filtro = Builders<Arbitros>.Filter.Eq(x => x.id, new MongoDB.Bson.ObjectId(id));
+            await collectin.DeleteOneAsync(filtro);
+        }
 
+        public async Task<Arbitros> GetArbitroById(string id)
+        {
+            Arbitros arbitros = new Arbitros();
+            try
+            {
+                arbitros = await collectin.FindAsync(new BsonDocument { { "_id", new ObjectId(id) } }).Result.FirstAsync();
+            }
+            catch (Exception e)
+            {
+                return arbitros;
+            }
+            return arbitros;
+        }
+
+        public async Task<List<Arbitros>> GetListArbitross()
+        {
+            return await collectin.FindAsync(new BsonDocument()).Result.ToListAsync();
+        }
+
+        public async Task InsertArbitro(Arbitros entidad)
+        {
+            entidad.fecha = DateTime.Now;
+            await collectin.InsertOneAsync(entidad);
+        }
+
+        public async Task UpdateArbitro(Arbitros entidad)
+        {
+            var filtro = Builders<Arbitros>.Filter.Eq(x => x.id, entidad.id);
+            entidad.fecha = DateTime.Now;
+            await collectin.ReplaceOneAsync(filtro, entidad);
+        }
     }
 }
