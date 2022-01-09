8ecd101f9cb23cf1193569b72bdedc748c147faa
MAURICIO BELLO <bello900@hotmail.com>
2022-01-08 22:08:23 -0500

    inicio replicacion implementacion frond-end

---
+++ WebFrontEnd/Controllers/ArbitrosController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebFrontEnd.Controllers
+{
+    public class ArbitrosController : Controller
+    {
+        public IActionResult GestionArbitros(string id)
+        {
+            ViewBag.idString = "";
+            if (!string.IsNullOrWhiteSpace(id))
+            {
+                ViewBag.idString = id;
+            }
+            return View();
+        }
+        public IActionResult GetArbitros()
+        {
+            return View();
+        }
+    }
+}
